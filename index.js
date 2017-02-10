9f307638ee6761ed49a0917efb2d63223869fa36
Diego Rodríguez <diegorbaquero@gmail.com>
2017-02-10 14:15:18 -0500

    v1.0.0

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-/**
- * Created by diego on 10/2/17.
- */
+const spawn = require('child_process').spawn
+const tapSpec = require('tap-spec')
+const glob = require('glob')
+const Readable = require('stream').Readable
+
+function Bogota (paths) {
+  const s = new Readable()
+  s._read = () => {}
+  s.pipe(tapSpec()).pipe(process.stdout)
+
+  let hm = paths.length
+
+  console.log(paths)
+  paths.forEach(path => {
+    glob(path, (err, files) => {
+      if (err) console.error(err)
+      hm += files.length
+      files.forEach(f => runTestFile(f))
+    })
+    hm--
+  })
+
+  function runTestFile (filename) {
+    const process = spawn('node', [filename])
+    const data = []
+    process.stdout.on('data', d => {
+      data.push(d)
+    })
+
+    process.stderr.on('data', err => {
+      console.error(err)
+    })
+
+    process.on('exit', () => {
+      s.push(data.join(''))
+      if (--hm === 0) s.push(null)
+    })
+  }
+}
+
+module.exports = Bogota
