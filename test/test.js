eaa49e18704d7bbe2373dcec93ed545fe8429616
Diego Rodríguez <diegorbaquero@gmail.com>
2017-02-22 02:20:44 -0500

    Random tests for testing

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
 const test = require('tape')
 
-test('Hello', t => {
+test('1 check', t => {
   t.plan(1)
   t.ok(true)
 })
 
-test('THis', t => {
+test('2 checks', t => {
   t.plan(2)
   t.ok(true)
   t.ok(true)
 })
 
-test('Is a test', t => {
+test('3 checks', t => {
   t.plan(3)
   t.ok(true)
   t.ok(true)
