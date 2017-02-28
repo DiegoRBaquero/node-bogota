a416271268137f3fd4b9e5ab5c646eb993dce086
Diego Rodríguez <diegorbaquero@gmail.com>
2017-02-28 14:10:12 -0500

    Better tests

--- test/test4.js
+++ test/test4.js
@@ -2,18 +2,30 @@ const test = require('tape')
 
 test('1 check', t => {
   t.plan(1)
-  t.ok(true)
+  setTimeout(() => {
+    t.ok(true)
+  }, 500)
 })
 
 test('2 checks', t => {
   t.plan(2)
-  t.ok(true)
-  t.ok(true)
+  setTimeout(() => {
+    t.ok(true)
+    setTimeout(() => {
+      t.ok(true)
+    }, 500)
+  }, 500)
 })
 
 test('3 checks', t => {
   t.plan(3)
-  t.ok(true)
-  t.ok(true)
-  t.ok(true)
+  setTimeout(() => {
+    t.ok(true)
+    setTimeout(() => {
+      t.ok(true)
+    }, 500)
+    setTimeout(() => {
+      t.ok(true)
+    }, 500)
+  }, 500)
 })
